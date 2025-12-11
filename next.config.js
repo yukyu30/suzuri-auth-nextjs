642c98a4c114815df675951f7f34111ad20b8e43
yu5 <fukanoyugo+dev@gmail.com>
2025-12-12 01:55:42 +0900

    fix

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.externals = [...config.externals, { canvas: 'canvas' }]; // Konva & react-konvaのために必要
-    return config;
-  },
-};
-
-module.exports = nextConfig; 
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  turbopack: {},
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: 'canvas' }];
+    return config;
+  },
+};
 
 export default nextConfig;
